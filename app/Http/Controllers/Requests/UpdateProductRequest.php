9b88b2ee5bc0324f2b514ce7dd31a2f179f1e9e1
MIS-ShSR <mhd.alaa.allawi@gmail.com>
2025-08-24 12:07:05 +0300

    Create UpdateProductRequest.php

---
+++ app/Http/Controllers/Requests/UpdateProductRequest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace App\Http\Requests;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+
+
+class UpdateProductRequest extends FormRequest
+{
+public function authorize(): bool
+{
+return true;
+}
+
+
+public function rules(): array
+{
+return [
+'name' => ['required','string','max:255'],
+'category' => ['nullable','string','max:255'],
+'description' => ['nullable','string'],
+'price' => ['required','numeric','min:0'],
+'stock' => ['required','integer','min:0'],
+'is_active' => ['nullable','boolean'],
+'image' => ['nullable','image','mimes:jpg,jpeg,png,webp','max:2048'],
+];
+}
+}
